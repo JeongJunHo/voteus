078a5bd5f2471663d2575ebe4379e7a62bc34732
JeongJunHo <jjh49470826@gmail.com>
2020-01-29 17:55:11 +0900

    투표리스트 수정

--- react/skeleton/src/pages/VoteList.jsx
+++ react/skeleton/src/pages/VoteList.jsx
@@ -1,25 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Layout from "../layout/Layout";
 
 import { makeStyles } from "@material-ui/core";
 
-import MaterialTable from "../components/main/MaterialTable";
+import VoteTable from "../components/vote/VoteTable";
 
-import Link from "@material-ui/core/Link";
 import { ViewContext } from "../context/ViewContext";
-import axios from "axios";
 
 const useStyles = makeStyles(theme => ({}));
 
 const VoteList = props => {
   const classes = useStyles();
 
-  const testData = { test: "test123", test2: "test4321" };
-
   return (
     <ViewContext.Provider value={{}}>
       <Layout>
-        <MaterialTable testData={testData} />
+        <VoteTable />
       </Layout>
     </ViewContext.Provider>
   );
