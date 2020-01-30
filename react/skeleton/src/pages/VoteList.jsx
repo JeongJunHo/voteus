bfb3fba15625c80e9b51d270953e777ecbff6525
JeongJunHo <jjh49470826@gmail.com>
2020-01-30 17:53:49 +0900

    [+] 투표 리스트 수정 useEffect를 통해 중분류 리스트 props 전달

--- react/skeleton/src/pages/VoteList.jsx
+++ react/skeleton/src/pages/VoteList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../layout/Layout";
 
 import { makeStyles } from "@material-ui/core";
@@ -7,15 +7,46 @@ import VoteTable from "../components/vote/VoteTable";
 
 import { ViewContext } from "../context/ViewContext";
 
+import LinearProgress from "@material-ui/core/LinearProgress";
+
+import axios from "axios";
+
 const useStyles = makeStyles(theme => ({}));
 
 const VoteList = props => {
   const classes = useStyles();
 
+  const [state, setState] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          "http://54.180.134.217:8080/api/middlepart/getMiddlepartAllList"
+        );
+        setState(response.data);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <>
+        <LinearProgress />
+      </>
+    );
+  }
+
   return (
     <ViewContext.Provider value={{}}>
       <Layout>
-        <VoteTable />
+        <VoteTable middlepart={state} />
       </Layout>
     </ViewContext.Provider>
   );
