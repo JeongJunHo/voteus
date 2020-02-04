a0babbba5189f503216c5aaac5bc3b75d5c01871
GyuReeKim <starkim96@naver.com>
2020-02-04 17:50:43 +0900

    [+] 투표 화면 수정
    Former-commit-id: 15700cf24ec60aadec365daf7f508caeac67022a

--- React_User/web-for-user/src/components/main/UserVoteList.jsx
+++ React_User/web-for-user/src/components/main/UserVoteList.jsx
@@ -1,22 +1,58 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect, useContext, Fragment } from 'react';
 
 import VoteListContext from '../../context/VoteListContext';
 
 const UserVoteList = (props) => {
+    const [loading, setLoading] = useState(null);
+    const [vote, setVote] = useState(null);
+
     const votelist = useContext(VoteListContext);
 
-    return (
-        <div>
-            {votelist !== null && votelist.map((vote) => {
-                const changeVoteNumber = () => {
-                    props.setVoteNumber(vote.code)
-                }
-                return (
-                    <button key={vote.code} onClick={changeVoteNumber}>{vote.name}</button>
-                )
-            })}
-        </div>
-    )
+    useEffect(()=>{
+        setLoading(true)
+        console.log(votelist)
+        if (votelist !== null) {
+            let vote = []
+            for (let eachvote of votelist.keys()) {
+                console.log(eachvote.code)
+                vote.push(eachvote)
+                setVote(vote)
+            }
+        }
+        setLoading(false)
+    }, [])
+
+    const test = () => {
+        console.log(votelist)
+        if (votelist !== null) {
+            for (let eachvote of votelist.keys()) {
+                console.log(eachvote.code)
+            }
+        }
+    }
+
+    if (loading === false) {        
+        return (
+            <Fragment>
+                {vote.map((each) => {
+                    const changeVoteNumber = () => {
+                        props.setVoteNumber(each.code)
+                    }
+
+                    return (
+                        <button key={each.code} onClick={changeVoteNumber}>{each.name}</button>
+                    )
+                })}
+                <button onClick={test}>test</button>
+            </Fragment>
+        )
+    } else {
+        return (
+            <Fragment>
+                로딩중
+            </Fragment>
+        )
+    }
 }
 
 export default UserVoteList;
--- React_User/web-for-user/src/components/main/VoteCandidateList.jsx
+++ React_User/web-for-user/src/components/main/VoteCandidateList.jsx
@@ -1,38 +1,38 @@
-import React, { useEffect, useState, useContext } from 'react';
-
-import CandidateListContext from '../../context/CandidateListContext';
+import React, { useEffect } from 'react';
 
 const VoteCandidateList = props => {
-    const [loading, setLoading] = useState(null);
-    const [candidate, setCandidate] = useState(null);
-
-    const candidatelist = useContext(CandidateListContext);
-
-    // // 수정중입니다.
-    // useEffect(() => {
-    //     if (candidatelist !== null) {
-    //         setLoading(true)
-    //         candidatelist.map((each) => (
-    //             setCandidate(each.name)
-    //         ))
-    //     }
-    // }, [])
-
-    const test = () => {
-        console.log(candidatelist.map((candidate)=>(candidate.name)))
-    }
+    // useEffect(()=>{
+    //     console.log(props.eachvotecandidatelist)
+    // })
 
     const back = () => {
         props.setVoteNumber(null)
     }
 
-    return(
-        <div>
-            {props.votenumber}
-            <button onClick={test}>test</button>
-            <button onClick={back}>back</button>
-        </div>
-    )
+    const test = () => {
+        console.log(props.eachvotecandidatelist)
+    }
+    
+    if (props.eachvotecandidatelist !== null) {
+        return (
+            <div>
+                {props.eachvotecandidatelist.map((candidate) => {
+                    return (
+                        <button key={candidate.code} onClick={back}>{candidate.name}</button>
+                    )
+                })}
+                <button onClick={back}>back</button>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                로딩중
+                <button onClick={test}>test</button>
+                <button onClick={back}>back</button>
+            </div>
+        )
+    }
 }
 
 export default VoteCandidateList;
