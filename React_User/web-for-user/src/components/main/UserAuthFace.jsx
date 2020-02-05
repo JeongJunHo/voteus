ebad3de1faa4ea5cce68dab28d22035185c18844
JeongJunHo <jjh49470826@gmail.com>
2020-02-05 18:00:11 +0900

    [+] 사용자 인증 페이지 얼굴 인증 페이지 개발
    Former-commit-id: 17e61406d77a5d3a8c5496fd8287a19a9aac6e9a

--- React_User/web-for-user/src/components/main/UserAuthFace.jsx
+++ React_User/web-for-user/src/components/main/UserAuthFace.jsx
@@ -1,57 +1,72 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment, useContext } from "react";
 
-import UserAuthFaceWebcam from './UserAuthFaceWebcam';
+import UserAuthFaceWebcam from "./UserAuthFaceWebcam";
+import UserNameContext from "../../context/UserNameContext";
+import Button from "@material-ui/core/Button";
 
 const UserAuthFace = props => {
-    const [result, setResult] = useState('face')
+  const [result, setResult] = useState("face");
+  const [capture, setCapture] = useState("capture");
+  const userName = useContext(UserNameContext);
 
-    const nextPage = () => {
-        // console.log(props.number)
-        props.setNumber(number => number + 1)
-    }
+  const nextPage = () => {
+    // console.log(props.number)
+    props.setNumber(number => number + 1);
+  };
 
-    const returnPage = () => {
-        setResult(result => 'face')
-    }
-    
-    if (result === 'set') {
-        return (
-            <Fragment>
-                <div>
-                    인증중...
-                </div>
-            </Fragment>
-        )
-    } else if (result === 'true') {
-        return (
-            <Fragment>
-                <div>
-                    인증이 완료되었습니다.
-                    <button onClick={nextPage}>다음</button>
-                </div>
-            </Fragment>
-        )
-    } else if (result === 'false') {
-        return (
-            <Fragment>
-                <div>
-                    인증이 실패되었습니다.
-                    다시 인증하세요.
-                    <button onClick={returnPage}>다시하기</button>
-                    {/* 처음 화면으로 돌아가는 버튼 필요 (이름 잘못 입력했을 경우) */}
-                </div>
-            </Fragment>
-        ) 
-    } else {
-        return (
-            <Fragment>
-                <h1>얼굴 인증</h1>
-                {props.number}
-                <UserAuthFaceWebcam result={result} setResult={setResult} />
-            </Fragment>
-        )
-    }
+  const returnPage = () => {
+    setResult(result => "face");
+  };
 
-}
+  if (result === "set") {
+    return (
+      <Fragment>
+        <div>인증중...</div>
+      </Fragment>
+    );
+  } else if (result === "true") {
+    return (
+      <Fragment>
+        <div>
+          <p>인증에 성공했습니다.</p>
+          <p>지문인증화면으로 진행됩니다.</p>
+          <Button
+            variant="contained"
+            size="large"
+            color="primary"
+            fullWidth="true"
+            onClick={nextPage}
+          >
+            다음
+          </Button>
+        </div>
+      </Fragment>
+    );
+  } else if (result === "false") {
+    return (
+      <Fragment>
+        <div>
+          인증이 실패되었습니다. 다시 인증하세요.
+          <Button
+            variant="contained"
+            size="large"
+            color="primary"
+            fullWidth="true"
+            onClick={returnPage}
+          >
+            다시하기
+          </Button>
+        </div>
+      </Fragment>
+    );
+  } else {
+    return (
+      <Fragment>
+        <h1>{userName}님 카메라를 응시해주세요.</h1>
+        <UserAuthFaceWebcam result={result} setResult={setResult} />
+      </Fragment>
+    );
+  }
+};
 
 export default UserAuthFace;
