7124e1e2fde874e6fb2ad0358598a982f60dadfa
JeongJunHo <jjh49470826@gmail.com>
2020-02-05 18:01:21 +0900

    [+] 인증 - 사용자 정보 입력 페이지 required 추가
    Former-commit-id: c3e6440be3765cb9314045bd4a2ad1601edd0f7a

--- React_User/web-for-user/src/components/main/UserAuthName.jsx
+++ React_User/web-for-user/src/components/main/UserAuthName.jsx
@@ -1,7 +1,6 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, Fragment, useMemo, useCallback } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
@@ -12,15 +11,7 @@ const useStyles = makeStyles(theme => ({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    height: 700
-    // backgroundColor: "red"
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  title: {
-    flexGrow: 1,
-    textAlign: "center"
+    height: "80vh"
   },
   paper: {
     padding: theme.spacing(2),
@@ -40,138 +31,180 @@ const useStyles = makeStyles(theme => ({
 const UserAuthName = props => {
   const classes = useStyles();
 
+  const [commit, setCommit] = useState(false);
   const [name, setName] = useState("");
   const [birth_registration_number, setBirthRegistrationNumber] = useState("");
   const [area_registration_number, setAreaRegistrationNumber] = useState("");
 
-  const handleChangeName = e => {
+  const nameError = useMemo(() => (name.length < 2 ? true : false), [name]);
+  const namePopup = useMemo(() => {
+    if (commit) {
+      return name.length >= 2 ? false : true;
+    } else {
+      return name.length === 0 || name.length >= 2 ? false : true;
+    }
+  }, [name, commit]);
+
+  const birthRegError = useMemo(
+    () => (birth_registration_number.length === 6 ? false : true),
+    [birth_registration_number]
+  );
+  const birthRegPopup = useMemo(() => {
+    if (commit) {
+      return birth_registration_number.length === 6 ? false : true;
+    } else {
+      return birth_registration_number.length === 0 ||
+        birth_registration_number.length === 6
+        ? false
+        : true;
+    }
+  }, [birth_registration_number, commit]);
+
+  const areaRegError = useMemo(
+    () => (area_registration_number.length === 7 ? false : true),
+    [area_registration_number]
+  );
+  const areaRegPopup = useMemo(() => {
+    if (commit) {
+      return area_registration_number.length === 7 ? false : true;
+    } else {
+      return area_registration_number.length === 0 ||
+        area_registration_number.length === 7
+        ? false
+        : true;
+    }
+  }, [area_registration_number, commit]);
+
+  const handleChangeName = useCallback(e => {
     setName(e.target.value);
-  };
+  }, []);
 
-  const handleChangeBirthRegistrationNumber = e => {
-    if (e.target.value.length <= 6) {
-      setBirthRegistrationNumber(e.target.value);
-    }
-  };
+  const handleChangeBirthRegistrationNumber = useCallback(e => {
+    setBirthRegistrationNumber(e.target.value);
+  }, []);
 
-  const handleChangeAreaRegistrationNumber = e => {
-    if (e.target.value.length <= 7) {
-      setAreaRegistrationNumber(e.target.value);
-    }
-  };
+  const handleChangeAreaRegistrationNumber = useCallback(e => {
+    setAreaRegistrationNumber(e.target.value);
+  }, []);
 
   const handleSubmit = e => {
     e.preventDefault();
+    setCommit(true);
 
     const registration_number =
       birth_registration_number + area_registration_number;
 
-    if (name.length < 2) {
-      alert("이름 입력");
-    } else if (
-      birth_registration_number.length !== 6 ||
-      area_registration_number.length !== 7
-    ) {
-      alert("주민번호 입력");
-    } else {
+    if (!nameError && !birthRegError && !areaRegError) {
       const data = {
-        code: registration_number,
+        id_num: registration_number,
         name: name
       };
 
       // axios
-      // axios.get('http://54.180.134.217:8080/api/vote/getVoteAllList/'+registration_number, data)
-      axios.get("주소" + registration_number, data).then(res => {
-        console.log(res);
-        if (res === "true") {
-          console.log(props.userinfo);
-          props.setUserInfo(userinfo => data);
-          props.setNumber(number => number + 1);
-        } else {
-          alert("정보가 일치하지 않습니다.");
-        }
-      });
-      props.setUserInfo(userinfo => data); // test
+      axios
+        .get(
+          "http://54.180.134.217:8080/api/voter/getOnlyVotercode/" +
+            name +
+            "/" +
+            registration_number
+        )
+        .then(res => {
+          console.log(res);
+          if (res.data != null) {
+            console.log(props.userinfo);
+            props.setUserInfo(data);
+            props.setNumber(number => number + 1);
+          } else {
+            alert("정보가 일치하지 않습니다.");
+          }
+        });
+      props.setUserInfo(data); // test
       props.setNumber(number => number + 1); // 임시(삭제 필요)
     }
   };
 
   return (
     <Fragment>
-      <Grid container className={classes.flex}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>
-            <h1>투표 시작 전 인증을 시작합니다.</h1>
-            <form onSubmit={handleSubmit}>
-              <Grid container spacing={3}>
-                <Grid item xs={12}>
-                  <TextField
-                    id="standard-full-width"
-                    label="이름"
-                    style={{ margin: 8 }}
-                    placeholder="이름을 입력해주세요."
-                    helperText="이름을 입력해주세요."
-                    fullWidth
-                    margin="normal"
-                    InputLabelProps={{
-                      shrink: true
-                    }}
-                    type="text"
-                    value={name}
-                    onChange={handleChangeName}
-                  />
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField
-                    id="standard-full-width"
-                    label="주민등록번호"
-                    style={{ margin: 8 }}
-                    placeholder="앞번호 6자리"
-                    helperText="주민등록번호 앞 6자리를 입력해주세요."
-                    fullWidth
-                    margin="normal"
-                    InputLabelProps={{
-                      shrink: true
-                    }}
-                    type="number"
-                    value={birth_registration_number}
-                    onChange={handleChangeBirthRegistrationNumber}
-                  />
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField
-                    id="standard-full-width"
-                    label=" "
-                    style={{ margin: 8 }}
-                    placeholder="뒷번호 7자리"
-                    helperText="주민등록번호 뒷 7자리를 입력해주세요."
-                    fullWidth
-                    margin="normal"
-                    InputLabelProps={{
-                      shrink: true
-                    }}
-                    type="number"
-                    value={area_registration_number}
-                    onChange={handleChangeAreaRegistrationNumber}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Button
-                    variant="contained"
-                    size="large"
-                    color="primary"
-                    className={classes.margin}
-                    fullWidth="true"
-                    type="submit"
-                  >
-                    인증
-                  </Button>
-                </Grid>
-              </Grid>
-            </form>
-          </Paper>
+      <h1>투표 시작 전 인증을 시작합니다.</h1>
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <TextField
+              id="standard-full-width"
+              label="이름"
+              placeholder="이름을 입력해주세요."
+              helperText={namePopup ? "2글자이상 입력해주세요." : " "}
+              fullWidth
+              margin="normal"
+              InputLabelProps={{
+                shrink: true
+              }}
+              type="text"
+              value={name}
+              onChange={handleChangeName}
+              autoComplete="off"
+              error={namePopup}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              id="standard-full-width"
+              label="주민등록번호"
+              placeholder="앞번호 6자리"
+              helperText={
+                birthRegPopup ? "주민등록번호 앞 6자리를 입력해주세요." : " "
+              }
+              fullWidth
+              margin="normal"
+              InputLabelProps={{
+                shrink: true
+              }}
+              type="text"
+              value={birth_registration_number}
+              onChange={handleChangeBirthRegistrationNumber}
+              autoComplete="off"
+              error={birthRegPopup}
+              inputProps={{
+                maxLength: 6
+              }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              id="standard-full-width"
+              label=" "
+              placeholder="뒷번호 7자리"
+              helperText={
+                areaRegPopup ? "주민등록번호 뒷 7자리를 입력해주세요." : " "
+              }
+              fullWidth
+              margin="normal"
+              InputLabelProps={{
+                shrink: true
+              }}
+              type="text"
+              value={area_registration_number}
+              onChange={handleChangeAreaRegistrationNumber}
+              autoComplete="off"
+              error={areaRegPopup}
+              inputProps={{
+                maxLength: 7
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              size="large"
+              color="primary"
+              fullWidth="true"
+              type="submit"
+            >
+              인증
+            </Button>
+          </Grid>
         </Grid>
-      </Grid>
+      </form>
     </Fragment>
   );
 };
