a6de9126ac6da8d5dfe5b4d9d8f61094053d6483
JeongJunHo <jjh49470826@gmail.com>
2020-02-06 16:27:15 +0900

    [+] 사용자 얼굴인증 화면 분기 수정
    Former-commit-id: 97b0b988eac68db0d43c46f6d6a0dc1a2977a24a

--- React_User/web-for-user/src/components/main/UserAuthFace.jsx
+++ React_User/web-for-user/src/components/main/UserAuthFace.jsx
@@ -1,13 +1,52 @@
-import React, { useState, Fragment, useContext } from "react";
+import React, { useState, Fragment } from "react";
 
+//custom component
 import UserAuthFaceWebcam from "./UserAuthFaceWebcam";
-import UserNameContext from "../../context/UserNameContext";
+
+//material-ui
+import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import Image from "material-ui-image";
+import Grid from "@material-ui/core/Grid";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import HelpIcon from "@material-ui/icons/Help";
+import IconButton from "@material-ui/core/IconButton";
+import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
+import HighlightOffIcon from "@material-ui/icons/HighlightOff";
+
+//image
+import finger from "../../images/finger.png";
+import captureExam from "../../images/captureExam.png";
+import profileDummy from "../../images/profileDummy.jpg";
+
+const useStyles = makeStyles(theme => ({
+  bold: {
+    fontWeight: "bold"
+  },
+  vh_80: {
+    height: "80vh"
+  }
+}));
 
 const UserAuthFace = props => {
+  const classes = useStyles();
+
   const [result, setResult] = useState("face");
   const [capture, setCapture] = useState("capture");
-  const userName = useContext(UserNameContext);
+  const [open, setOpen] = React.useState(true);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const nextPage = () => {
     // console.log(props.number)
@@ -22,48 +61,134 @@ const UserAuthFace = props => {
     return (
       <Fragment>
         <div>인증중...</div>
+        <CircularProgress />
       </Fragment>
     );
   } else if (result === "true") {
     return (
       <Fragment>
-        <div>
-          <p>인증에 성공했습니다.</p>
-          <p>지문인증화면으로 진행됩니다.</p>
-          <Button
-            variant="contained"
-            size="large"
-            color="primary"
-            fullWidth="true"
-            onClick={nextPage}
-          >
-            다음
-          </Button>
-        </div>
+        <h1>인증에 성공했습니다.</h1>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Image
+              src={finger}
+              alt="finger-print"
+              disableSpinner="true"
+              disableTransition="true"
+              style={{ height: "200px", paddingTop: 0 }}
+              imageStyle={{ width: "auto", position: "static" }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <p>지문인증절차를 진행합니다.</p>
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              size="large"
+              color="primary"
+              fullWidth="true"
+              onClick={nextPage}
+            >
+              다음
+            </Button>
+          </Grid>
+        </Grid>
       </Fragment>
     );
   } else if (result === "false") {
     return (
-      <Fragment>
-        <div>
-          인증이 실패되었습니다. 다시 인증하세요.
-          <Button
-            variant="contained"
-            size="large"
-            color="primary"
-            fullWidth="true"
-            onClick={returnPage}
-          >
-            다시하기
-          </Button>
-        </div>
-      </Fragment>
+      <>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <h1>인증에 실패했습니다.</h1>
+          </Grid>
+          <Grid item xs={12}>
+            <Image
+              src={captureExam}
+              alt="finger-print"
+              disableSpinner="true"
+              disableTransition="true"
+              style={{ height: "200px", paddingTop: 0 }}
+              imageStyle={{ width: "auto", position: "static" }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <p>상단의 사진을 참고하여 재촬영을 진행해주세요!</p>
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              size="large"
+              color="primary"
+              fullWidth="true"
+              onClick={returnPage}
+            >
+              재촬영
+            </Button>
+          </Grid>
+        </Grid>
+      </>
     );
   } else {
     return (
       <Fragment>
-        <h1>{userName}님 카메라를 응시해주세요.</h1>
+        <IconButton
+          color="secondary"
+          aria-label="도움말"
+          onClick={handleClickOpen}
+        >
+          <HelpIcon fontSize="large" />
+          도움말
+        </IconButton>
+
         <UserAuthFaceWebcam result={result} setResult={setResult} />
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+          fullWidth="true"
+          PaperProps={{ className: [classes.vh_80] }}
+        >
+          <DialogTitle id="alert-dialog-title">촬영 전 확인사항</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              <h1>올바른 촬영 자세</h1>
+              <CheckCircleOutlineIcon fontSize="large" color="primary" />
+              <Image
+                src={profileDummy}
+                alt="finger-print"
+                disableSpinner="true"
+                disableTransition="true"
+                style={{ height: "200px", paddingTop: 0 }}
+                imageStyle={{ width: "auto", position: "static" }}
+              />
+              <h1>잘못된 촬영 자세</h1>
+              <HighlightOffIcon fontSize="large" color="secondary" />
+              <Image
+                src={profileDummy}
+                alt="finger-print"
+                disableSpinner="true"
+                disableTransition="true"
+                style={{ height: "200px", paddingTop: 0 }}
+                imageStyle={{ width: "auto", position: "static" }}
+              />
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              variant="contained"
+              size="large"
+              color="primary"
+              fullWidth="true"
+              onClick={handleClose}
+              autoFocus
+            >
+              확인
+            </Button>
+          </DialogActions>
+        </Dialog>
       </Fragment>
     );
   }
