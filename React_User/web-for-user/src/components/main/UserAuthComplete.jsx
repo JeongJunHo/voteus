eef04764a69ea12916b170de848e04a81bc8dc3b
JeongJunHo <jjh49470826@gmail.com>
2020-02-06 18:01:07 +0900

    [+] 코드 정리
    Former-commit-id: 310da183f510c17413cb24493da095df52203018

--- React_User/web-for-user/src/components/main/UserAuthComplete.jsx
+++ React_User/web-for-user/src/components/main/UserAuthComplete.jsx
@@ -1,17 +1,22 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
+import { Link } from "@material-ui/core";
 
-const UserAuthComplete = (props) => {
-    const url = "/user-vote/" + props.userinfocode;
+const UserAuthComplete = props => {
+  const url = "/user-vote/" + props.userinfocode;
 
-    return (
-        <Fragment>
-            <h1>모든인증완료</h1>
-            <p>투표를 해주세요</p>
-            <Button variant="contained" color="primary" href={url}>투표 시작</Button>
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <h1>모든인증완료</h1>
+      <p>투표를 해주세요</p>
+      <Link href={url}>
+        <Button variant="contained" color="primary">
+          투표 시작
+        </Button>
+      </Link>
+    </Fragment>
+  );
+};
 
 export default UserAuthComplete;
