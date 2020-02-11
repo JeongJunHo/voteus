7d23b3695f2ffb46739e5afdc3d7eb0142b8b6d2
JJongSu <jspark940321@gmail.com>
2020-02-11 10:54:50 +0900

    [+] add & delete file 주석 추가 및 불필요파일 삭제
    Former-commit-id: bda0414ad4f85b67b41e458a0795ea526d093142

--- Flask_Practice/start_flask.py
+++ Flask_Practice/start_flask.py
@@ -25,13 +25,23 @@ def getData():
     data = json.dumps(b)
     return data
 
+'''
+지문센서에서 지문 데이터를 인식하고, 그 데이터를 저장하고, react에 전달하는 코드
+example_downloadimage에서 fingerprint.bmp로 지문을 저장하는 코드
+
+박종수
+
+00 성공
+01 실패
+03 시간초과
+'''
 @app.route('/getFinger', methods=['post'])
 def getFinger():
     is_true = ed.get_finger()
     print(is_true)
     print(type(is_true))
 
-    if is_true == True:
+    if is_true == "00":
         with open('fingerprint.bmp', 'rb') as image:
             b64img = base64.b64encode(image.read())
         #print(b64img)
@@ -39,48 +49,61 @@ def getFinger():
         print(type(b64img))
         #data = {"code": "05", "img": str(b64img)}
 
-        data = json.dumps({"code": "05", "img": str(b64img)[2:-1]})
+        data = json.dumps({"code": "00", "img": str(b64img)[2:-1]})
         #data = json.dumps(True)
     else:
-        data = json.dumps({"code" : "04"})
+        data = json.dumps({"code" : is_true})
         #data = json.dumps (False)
-
     return data
 
+'''
+react로 부터 사진(base64)과 이름(사람코드)를 받아서 일치여부를 확인해 주는 코드
+knn_ImageFile_TF에서 knn을 통해 이미지를 분류받아 사람코드를 return받고 일치여부를 code에 담아 전달
+code 00 일치
+code 01 불일치
+code 02 에러발생
+박종수
+'''
 @app.route('/getImg', methods=['post'])
 def put_img():
     print(1)
-    data = request.get_json()
-    name = data['name']
-    print(name)
-    #print(data)
-    img = data['img']
-    #print(img)
-    #print(request.files['img'])
-    #f = request.files['img']
-    file = base64.b64decode(str(img).split(',')[1])
-    # print("file : ")
-    # print(file)
-    # # f = Image.open(io.BytesIO(file))
-    filename = 'find.jpg'
-    # f = open(filename, 'wb')
-    # f.write(file)
-    with open(filename, 'wb') as f:
-        f.write(file)
-    print(2)
-    # print(f)
-    file = open('find.jpg', 'r')
-    tmpname = knn.get_name(file)
-    print('tmpname : ' + tmpname)
-    if tmpname==name:
-        data = json.dumps(True)
-    else:
-        data = json.dumps(False)
-    #data = json.dumps()
-    print(3)
-    return data
 
-    #return data
+    try:
+        # img와 코드를 저장
+        data = request.get_json()
+        name = data['name']
+        print(name)
+        img = data['img']
+
+        # base를 디코딩하고 find.jpg로 저장
+        file = base64.b64decode(str(img).split(',')[1])
+        filename = 'find.jpg'
+        with open(filename, 'wb') as f:
+            f.write(file)
+        print(2)
+        file = open('find.jpg', 'r')
+
+        # tmpname에 knn으로 부터 얻은 코드를 저장
+        tmpname = knn.get_name(file)
+        print('tmpname : ' + tmpname)
+
+        # react로 받은 코드와 knn에서 나온 사람코드 값을 비교하여 일치여부를 json 파일로 저장 후 code에
+        if tmpname == name:
+            data = json.dumps({"code": "00"})
+        else:
+            data = json.dumps({"code": "01"})
+        # data = json.dumps()
+        print(3)
+        return data
+
+    except Exception as e:
+        print('Operation failed!')
+        print('Exception message: ' + str(e))
+        data = json.dumps({"code": "02"})
+        return data
+
+
+
 
 @app.route('/getJJ', methods=['post'])
 def jj():
