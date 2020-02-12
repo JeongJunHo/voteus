64a7039759f4331b6c1abddb4e98fecfb2c6b80a
JeongJunHo <jjh49470826@gmail.com>
2020-02-12 17:44:44 +0900

    [+] chart dummy 데이터 삭제 및 자동 새로고침 기능 추가
    Former-commit-id: a0a326ab39c09d834fef789fca127210e18a9cb9

--- react/skeleton/src/components/main/Chart.jsx
+++ react/skeleton/src/components/main/Chart.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import {
   BarChart,
   Bar,
+  Brush,
+  ReferenceLine,
   XAxis,
   YAxis,
   CartesianGrid,
@@ -13,203 +15,78 @@ import LinearProgress from "@material-ui/core/LinearProgress";
 import axios from "axios";
 import { date_to_str_onlyDate } from "../../common.js";
 
-const test = [
-  {
-    name: "01:00",
-    후보1: 4000,
-    후보2: 2400,
-    후보3: 2400
-  },
-  {
-    name: "02:00",
-    후보1: 3000,
-    후보2: 1398,
-    후보3: 2210
-  },
-  {
-    name: "03:00",
-    후보1: 2000,
-    후보2: 9800,
-    후보3: 2290
-  },
-  {
-    name: "04:00",
-    후보1: 2780,
-    후보2: 3908,
-    후보3: 2000
-  },
-  {
-    name: "05:00",
-    후보1: 1890,
-    후보2: 4800,
-    후보3: 2181
-  },
-  {
-    name: "06:00",
-    후보1: 2390,
-    후보2: 3800,
-    후보3: 2500
-  },
-  {
-    name: "07:00",
-    후보1: 3490,
-    후보2: 4300,
-    후보3: 2100
-  },
-  {
-    name: "08:00",
-    후보1: 4000,
-    후보2: 2400,
-    후보3: 2400
-  },
-  {
-    name: "09:00",
-    후보1: 3000,
-    후보2: 1398,
-    후보3: 2210
-  },
-  {
-    name: "10:00",
-    후보1: 2000,
-    후보2: 9800,
-    후보3: 2290
-  },
-  {
-    name: "11:00",
-    후보1: 2780,
-    후보2: 3908,
-    후보3: 2000
-  },
-  {
-    name: "12:00",
-    후보1: 1890,
-    후보2: 4800,
-    후보3: 2181
-  },
-  {
-    name: "13:00",
-    후보1: 2390,
-    후보2: 3800,
-    후보3: 2500
-  },
-  {
-    name: "14:00",
-    후보1: 3490,
-    후보2: 4300,
-    후보3: 2100
-  },
-  {
-    name: "15:00",
-    후보1: 4000,
-    후보2: 2400,
-    후보3: 2400
-  },
-  {
-    name: "16:00",
-    후보1: 3000,
-    후보2: 1398,
-    후보3: 2210
-  },
-  {
-    name: "17:00",
-    후보1: 2000,
-    후보2: 9800,
-    후보3: 2290
-  },
-  {
-    name: "18:00",
-    후보1: 2780,
-    후보2: 3908,
-    후보3: 2000
-  },
-  {
-    name: "19:00",
-    후보1: 1890,
-    후보2: 4800,
-    후보3: 2181
-  },
-  {
-    name: "20:00",
-    후보1: 2390,
-    후보2: 3800,
-    후보3: 2500
-  },
-  {
-    name: "21:00",
-    후보1: 3490,
-    후보2: 4300,
-    후보3: 2100
-  },
-  {
-    name: "22:00",
-    후보1: 3490,
-    후보2: 4300,
-    후보3: 2100
-  },
-  {
-    name: "23:00",
-    후보1: 3490,
-    후보2: 4300,
-    후보3: 2100
-  }
-];
-
 const Chart = props => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
   const [candidate, setCandidate] = useState([]);
+  const [refresh, setRefresh] = useState(false);
+
   useEffect(() => {
     const [voteCode] = props.selected;
-    console.log("voteCode다", voteCode);
     const fetchData = async () => {
       setLoading(true);
       const nowDate = date_to_str_onlyDate(new Date());
-      console.log(nowDate);
-      try {
-        const response = await axios.get(
-          "http://54.180.134.217:8080/api/statistics/getCandiPick/" +
-            voteCode +
-            "/" +
-            nowDate +
-            "/14"
-        );
-        const candiResponse = await axios.get(
-          "http://54.180.134.217:8080/api/candi/getCandiVotecode/" + voteCode
-        );
-        // setState(response.data);
-        setCandidate(candiResponse.data);
-        console.log("시간대별투표정보::");
-        console.log(response.data);
-        console.log("후보자::");
-        console.log(candidate);
-        let tmp = [];
-        for (let i = 0; i < 24; i++) {
-          tmp.push({ name: (i < 10 ? "0" + i : i) + ":00" });
+      let tmp = [];
+      for (let i = 0; i < 24; i++) {
+        tmp.push({ name: (i < 10 ? "0" + i : i) + ":00", time: i });
+      }
+
+      if (voteCode != null) {
+        try {
+          const candiResponse = await axios.get(
+            "http://54.180.134.217:8080/api/candi/getCandiVotecode/" + voteCode
+          );
+
+          if (candiResponse.data !== "failure") {
+            setCandidate(candiResponse.data);
+            for (const item of tmp) {
+              for (const candi of candiResponse.data) {
+                item[candi.name] = 0;
+              }
+            }
+          }
+
+          for (const item of tmp) {
+            const response = await axios.get(
+              "http://54.180.134.217:8080/api/statistics/getCandiPick/" +
+                voteCode +
+                "/" +
+                nowDate +
+                "/" +
+                item.time
+            );
+
+            if (response.data.length > 0) {
+              for (const score of response.data) {
+                item[score.name] += score.pick;
+              }
+            }
+          }
+
+          setData(tmp);
+        } catch (e) {
+          console.log(e);
         }
-        setData(tmp);
-        console.log("확인::");
-        console.log(tmp);
-      } catch (e) {
-        console.log(e);
+      } else {
+        setCandidate([]);
       }
       setLoading(false);
     };
     fetchData();
-  }, [props.selected]);
+  }, [props.selected, refresh]);
 
-  if (loading) {
-    return (
-      <>
-        <LinearProgress />
-      </>
-    );
-  }
+  const timer = setTimeout(() => {
+    setRefresh(refresh ? false : true);
+    console.log("타이머작동");
+  }, 20000);
 
   return (
     <>
-      <h1>선거 모니터링</h1>
-      {data.length > 0 ? (
-        <ResponsiveContainer width="100%" height={400}>
+      <h1>선거 모니터링 (시간대별 득표수)</h1>
+      {loading || data.length === 0 ? (
+        <LinearProgress />
+      ) : (
+        <ResponsiveContainer width="100%" height={300}>
           <BarChart
             data={data}
             margin={{
@@ -221,17 +98,23 @@ const Chart = props => {
           >
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="name" />
-            <YAxis type="number" domain={[0, "auto"]} />
             <Tooltip />
-            <Legend />
-
-            <Bar dataKey="후보1" label />
-            <Bar dataKey="후보2" fill="#82ca9d" />
-            <Bar dataKey="후보3" fill="#82caff" />
+            <Legend verticalAlign="top" />
+            <ReferenceLine y={0} stroke="#000" />
+            <YAxis type="number" domain={[0, "auto"]} allowDecimals={false} />
+            {candidate.map(candi => (
+              <Bar
+                key={candi.code}
+                dataKey={candi.name}
+                fill={`#${Math.round(Math.random() * 0xffffff).toString(16)}`}
+              />
+            ))}
+            {/* <Bar dataKey="선한빛" fill="#82ca00" />
+            <Bar dataKey="장인호" fill="#82ca9d" />
+            <Bar dataKey="박종수" fill="#82caff" /> */}
+            <Brush dataKey="name" height={50} stroke="#8884d8" />
           </BarChart>
         </ResponsiveContainer>
-      ) : (
-        <p>투표 정보가 없습니다.</p>
       )}
     </>
   );
