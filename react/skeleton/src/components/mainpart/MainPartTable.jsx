c82deac8c27f1406e5fe97863aca76687f34c4ff
JeongJunHo <jjh49470826@gmail.com>
2020-02-14 15:19:03 +0900

    [+] axios token 추가(미구현)
    Former-commit-id: 05e46884da03ca04f3c6c794b8d526fd9396fa28

--- react/skeleton/src/components/mainpart/MainPartTable.jsx
+++ react/skeleton/src/components/mainpart/MainPartTable.jsx
@@ -35,9 +35,12 @@ export default function MainPartTable() {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const response = await axios.get(
-          "http://54.180.134.217:8080/api/mainpart/getMainpartAllList"
-        );
+        const token = "Bearer " + sessionStorage.getItem("token");
+        const response = await axios({
+          method: "get",
+          url: "http://54.180.134.217:8080/api/mainpart/getMainpartAllList",
+          headers: { Authorization: token }
+        });
         const restate = {
           ...state,
           data: response.data
