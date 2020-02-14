2ca67ed5e090c1837dae0cd897137da133beb652
JeongJunHo <jjh49470826@gmail.com>
2020-02-14 16:06:55 +0900

    [+] swagger security 추가
    Former-commit-id: cd67b8866d447e237757eeb92c53357691ebd679

--- project01/VoteOn/src/main/java/com/ssafy/vote/WebSecurityConfig.java
+++ project01/VoteOn/src/main/java/com/ssafy/vote/WebSecurityConfig.java
@@ -50,9 +50,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		httpSecurity
 				.csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/authenticate","/api/**").permitAll().
+				.authorizeRequests().antMatchers(
+						"/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**",
+                        "/authenticate",
+                        "/api/**").permitAll()
 				// all other requests need to be authenticated
-				anyRequest().authenticated().and()
+				.anyRequest().authenticated()
+				.and()
 				.cors().and()
 				// make sure we use stateless session; session won't be used to
 				// store user's state.
