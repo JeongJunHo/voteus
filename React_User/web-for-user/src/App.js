bd6327fb0c92f54d336c5eaa843456e1e842f714
GyuReeKim <starkim96@naver.com>
2020-02-20 11:50:39 +0900

    [+] 404 page
    Former-commit-id: 7fddd33eb5da6f73e23580ede2b92c6ae4ac4c68

--- React_User/web-for-user/src/App.js
+++ React_User/web-for-user/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 import "./App.css";
 
 import UserHome from "./pages/UserHome";
 import UserAuthentication from "./pages/UserAuthentication";
 import UserVote from "./pages/UserVote";
+import PageNotFound from "./pages/PageNotFound";
+
 
 // import TypeContext from "./context/TypeContext";
 
@@ -15,11 +17,13 @@ class App extends Component {
       <Router>
         <Switch>
           <Route exact path="/" component={UserHome} />
-            <Route path="/user-home" component={UserHome} />
-            {/* <TypeContext.Provider value={'auth'}> */}
-            <Route path="/user-auth" component={UserAuthentication} />
-            {/* </TypeContext.Provider> */}
-            <Route path="/user-vote/:code/:name" component={UserVote} />
+          <Route path="/user-home" component={UserHome} />
+          {/* <TypeContext.Provider value={'auth'}> */}
+          <Route path="/user-auth" component={UserAuthentication} />
+          {/* </TypeContext.Provider> */}
+          <Route path="/user-vote/:code/:name" component={UserVote} />
+          <Route path="/page-not-found" component={PageNotFound} />
+          <Redirect to="/page-not-found" />
         </Switch>
       </Router>
     );
